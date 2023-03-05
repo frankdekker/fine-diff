567ece82bcb5123f4d0b269ac35fdcf03a1ada73
Frank Dekker <fdekker@123inkt.nl>
2023-03-05 13:51:56 +0100

    Update interfaces

--- src/Diff/Iterable/AbstractChangeDiffIterable.php
+++ src/Diff/Iterable/AbstractChangeDiffIterable.php
@@ -3,36 +3,17 @@ declare(strict_types=1);
 
 namespace FDekker\Diff\Iterable;
 
-use FDekker\Entity\Range;
-
-/**
- * Represents computed differences between two sequences.
- * <p>
- * All {@link Range} are not empty (have at least one element in one of the sides). Ranges do not overlap.
- * <p>
- * Differences are guaranteed to be 'squashed': there are no two changed or two unchanged {@link Range} with
- * <code>(range1.end1 == range2.start1 && range1.end2 == range2.start2)</code>.
- * @see FairDiffIterableInterface
- * @see DiffIterableUtil::iterateAll(DiffIterable)
- * @see DiffIterableUtil::verify(DiffIterable)
- */
 abstract class AbstractChangeDiffIterable implements DiffIterableInterface
 {
     public function __construct(private readonly int $length1, private readonly int $length2)
     {
     }
 
-    /**
-     * @return int length of the first sequence
-     */
     public function getLength1(): int
     {
         return $this->length1;
     }
 
-    /**
-     * @return int length of the second sequence
-     */
     public function getLength2(): int
     {
         return $this->length2;
