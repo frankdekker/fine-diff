fe693751f71ad7a9890d7681399beedd8c77c9d2
Frank Dekker <fdekker@123inkt.nl>
2023-03-05 13:37:09 +0100

    Add iterable classes

--- src/Diff/DiffIterableUtil.php
+++ src/Diff/DiffIterableUtil.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 namespace FDekker\Diff;
 
 use FDekker\Diff;
+use FDekker\Diff\Iterable\DiffChangeDiffIterable;
+use FDekker\Diff\Iterable\DiffIterableInterface;
+use FDekker\Diff\Iterable\FairDiffIterableInterface;
+use FDekker\Diff\Iterable\FairDiffIterableWrapper;
+use FDekker\Entity\Change;
 
 class DiffIterableUtil
 {
@@ -14,8 +19,24 @@ class DiffIterableUtil
     {
         try {
             $change = (new Diff())->buildChanges($chunks1, $chunks2);
+
+            return self::fair(self::create($change, count($chunks1), count($chunks2)));
         } catch (FilesTooBigForDiffException $e) {
             throw new DiffToBigException(previous: $e);
         }
+    }
+
+    public static function create(Change $change, int $length1, int $length2): DiffIterableInterface
+    {
+        return new DiffChangeDiffIterable($change, $length1, $length2);
+    }
+
+    public static function fair(DiffIterableInterface $iterable): FairDiffIterableInterface
+    {
+        if ($iterable instanceof FairDiffIterableInterface) {
+            return $iterable;
+        }
+
+        return new FairDiffIterableWrapper($iterable);
     }
 }
