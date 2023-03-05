76875fa29a2f3581946ee04312146bea1b5fb8c7
Frank Dekker <fdekker@123inkt.nl>
2023-03-05 21:12:27 +0100

    Add coverage

---
+++ tests/Unit/Util/TrimUtilTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace FDekker\Tests\Util;
+
+use FDekker\Util\TrimUtil;
+use PHPUnit\Framework\TestCase;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(TrimUtil::class)]
+class TrimUtilTest extends TestCase
+{
+    public function testExpandBackward(): void
+    {
+        // match [AB] from the start
+        $data1 = ['a', 'b', 'c', 'd'];
+        $data2 = ['d', 'a', 'b', 'f'];
+
+        static::assertSame(2, TrimUtil::expandForward($data1, $data2, 0, 1, count($data1), count($data2) - 1));
+    }
+
+    public function testExpandForward(): void
+    {
+        // match [AB] from the end
+        $data1 = ['d', 'c', 'b', 'a'];
+        $data2 = ['f', 'b', 'a', 'd'];
+
+        static::assertSame(2, TrimUtil::expandBackward($data1, $data2, 0, 0, count($data1), count($data2) - 1));
+    }
+}
