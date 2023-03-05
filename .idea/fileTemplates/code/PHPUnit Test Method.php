1eb2697ef61e165c585d8b1064b51eceee606bdb
Frank Dekker <fdekker@123inkt.nl>
2023-03-05 13:49:59 +0100

    Add coverage

--- .idea/fileTemplates/code/PHPUnit Test Method.php
+++
@@ -1,7 +0,0 @@
-/**
- * @covers ::${NAME}
- */
-public function test${CAPITALIZED_NAME}(): void
-{
-
-}
---
+++ tests/Unit/Diff/Iterable/DiffChangeChangeIterableTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace FDekker\Tests\Diff\Iterable;
+
+use FDekker\Diff\Iterable\DiffChangeChangeIterable;
+use FDekker\Entity\Change;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(DiffChangeChangeIterable::class)]
+class DiffChangeChangeIterableTest extends TestCase
+{
+    public function testIterator(): void
+    {
+        $changeA       = new Change(10, 20, 30, 40);
+        $changeB       = new Change(50, 60, 70, 80);
+        $changeA->link = $changeB;
+
+        $iterator = new DiffChangeChangeIterable($changeA);
+
+        // first iteration
+        static::assertTrue($iterator->valid());
+        static::assertSame(10, $iterator->getStart1());
+        static::assertSame(20, $iterator->getStart2());
+        static::assertSame(10 + 30, $iterator->getEnd1());
+        static::assertSame(20 + 40, $iterator->getEnd2());
+
+        // second iteration
+        $iterator->next();
+        static::assertTrue($iterator->valid());
+        static::assertSame(50, $iterator->getStart1());
+        static::assertSame(60, $iterator->getStart2());
+        static::assertSame(50 + 70, $iterator->getEnd1());
+        static::assertSame(60 + 80, $iterator->getEnd2());
+
+        // third iteration
+        $iterator->next();
+        static::assertFalse($iterator->valid());
+    }
+}
