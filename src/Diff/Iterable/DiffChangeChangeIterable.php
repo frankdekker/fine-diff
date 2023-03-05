fa8ea6ea79b2eab1e5066449d090002fc8e30476
Frank Dekker <fdekker@123inkt.nl>
2023-03-05 10:35:51 +0100

    Add iterable classes and phpstan fixes

--- src/Diff/Iterable/DiffChangeChangeIterable.php
+++ src/Diff/Iterable/DiffChangeChangeIterable.php
@@ -18,26 +18,31 @@ class DiffChangeChangeIterable implements ChangeIterableInterface
 
     public function next(): void
     {
+        assert($this->change !== null);
         $this->change = $this->change->link;
     }
 
     public function getStart1(): int
     {
+        assert($this->change !== null);
         return $this->change->line0;
     }
 
     public function getStart2(): int
     {
+        assert($this->change !== null);
         return $this->change->line1;
     }
 
     public function getEnd1(): int
     {
+        assert($this->change !== null);
         return $this->change->line0 + $this->change->deleted;
     }
 
     public function getEnd2(): int
     {
+        assert($this->change !== null);
         return $this->change->line1 + $this->change->inserted;
     }
 }
