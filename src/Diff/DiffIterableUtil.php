0ac382765b4c98b60cb5d25db3540f8060895dcd
Frank Dekker <fdekker@123inkt.nl>
2023-03-12 16:50:57 +0100

    Update license

--- src/Diff/DiffIterableUtil.php
+++ src/Diff/DiffIterableUtil.php
@@ -1,6 +1,6 @@
 <?php
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-// Copyright 2023 123inkt. Use of this source code is governed by the Apache 2.0 license.
+// Modification copyright 2023 123inkt. Use of this source code is governed by the Apache 2.0 license.
 declare(strict_types=1);
 
 namespace DR\JBDiff\Diff;
