f777a147f97776c0b850b5e7a0a405558a6d1861
Frank Dekker <fdekker@123inkt.nl>
2023-03-13 18:01:58 +0100

    Update license

--- src/Comparison/DefaultCorrector.php
+++ src/Comparison/DefaultCorrector.php
@@ -1,6 +1,6 @@
 <?php
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-// Modification copyright 2023 123inkt. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2023 Digital Revolution BV (123inkt.nl). Use of this source code is governed by the Apache 2.0 license.
 declare(strict_types=1);
 
 namespace DR\JBDiff\Comparison;
