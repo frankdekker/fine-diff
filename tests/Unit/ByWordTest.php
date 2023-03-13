5e7450429d356612d8a075a2d62ce885f07c423d
Frank Dekker <fdekker@123inkt.nl>
2023-03-13 19:50:23 +0100

    Fix test ByWord

--- tests/Unit/ByWordTest.php
+++ tests/Unit/ByWordTest.php
@@ -3,37 +3,38 @@ declare(strict_types=1);
 
 namespace DR\JBDiff\Tests\Unit;
 
-use DR\JBDiff\ByWord;
+use DR\JBDiff\ByWordRt;
+use DR\JBDiff\Entity\Character\CharSequence as CS;
 use DR\JBDiff\Entity\Chunk\NewLineChunk;
 use DR\JBDiff\Entity\Chunk\WordChunk;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
-#[CoversClass(ByWord::class)]
+#[CoversClass(ByWordRt::class)]
 class ByWordTest extends TestCase
 {
     public function testGetInlineChunksTwoWords(): void
     {
-        $text     = "public int";
+        $text     = CS::fromString("public int");
         $expected = [
-            new WordChunk($text, 0, 6, 3317543529),
-            new WordChunk($text, 7, 10, 104431),
+            new WordChunk($text, 0, 6),
+            new WordChunk($text, 7, 10),
         ];
 
-        $chunks = ByWord::getInlineChunks($text);
+        $chunks = ByWordRt::getInlineChunks($text);
         static::assertEquals($expected, $chunks);
     }
 
     public function testGetInlineChunksSpecialCharacter(): void
     {
-        $text     = "public int codë() {";
+        $text     = CS::fromString("public int codë() {");
         $expected = [
-            new WordChunk($text, 0, 6, 3317543529),
-            new WordChunk($text, 7, 10, 104431),
-            new WordChunk($text, 11, 15, 3059315),
+            new WordChunk($text, 0, 6),
+            new WordChunk($text, 7, 10),
+            new WordChunk($text, 11, 15),
         ];
 
-        $chunks = ByWord::getInlineChunks($text);
+        $chunks = ByWordRt::getInlineChunks($text);
         static::assertEquals($expected, $chunks);
 
         /** @var WordChunk $chunk */
@@ -43,31 +44,31 @@ class ByWordTest extends TestCase
 
     public function testGetInlineChunksNewLinesA(): void
     {
-        $text     = "public {\ntest";
+        $text     = CS::fromString("public {\ntest");
         $expected = [
-            new WordChunk($text, 0, 6, 3317543529),
+            new WordChunk($text, 0, 6),
             new NewLineChunk(8),
-            new WordChunk($text, 9, 13, 3556498),
+            new WordChunk($text, 9, 13),
         ];
 
-        $chunks = ByWord::getInlineChunks($text);
+        $chunks = ByWordRt::getInlineChunks($text);
         static::assertEquals($expected, $chunks);
     }
 
     public function testGetInlineChunksNewLines(): void
     {
-        $text     = "public int codë() {\ntest\n}\n";
+        $text     = CS::fromString("public int codë() {\ntest\n}\n");
         $expected = [
-            new WordChunk($text, 0, 6, 3317543529),
-            new WordChunk($text, 7, 10, 104431),
-            new WordChunk($text, 11, 15, 3059315),
+            new WordChunk($text, 0, 6),
+            new WordChunk($text, 7, 10),
+            new WordChunk($text, 11, 15),
             new NewLineChunk(19),
-            new WordChunk($text, 20, 24, 3556498),
+            new WordChunk($text, 20, 24),
             new NewLineChunk(24),
             new NewLineChunk(26),
         ];
 
-        $chunks = ByWord::getInlineChunks($text);
+        $chunks = ByWordRt::getInlineChunks($text);
         static::assertEquals($expected, $chunks);
     }
 }
